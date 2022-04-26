621ff3de64335dd7845a7812535995d01427c3ff
Ниязбек Аманжолов <47739624+NiyazbekAmanzholov@users.noreply.github.com>
2022-04-26 18:40:45 +0600

    Update Main.java
    //1111111111111111111111111

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaa");
+        //1111111111111111111111111
+        //1111111111111111111111111
+        //1111111111111111111111111
     }
 }
